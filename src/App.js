2a20b8df0eb47311758a8ead8c715c53bb75608b
unknown <sprout20091008@gmail.com>
2023-08-20 10:08:44 +0900

    feat: フッタの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import BasicHeader from './components/header/BasicHeader';
+import BasicFooter from './components/footer/BasicFooter';
 
 function App() {
   return (
     <div className="App">
       <BasicHeader />
+      <BasicFooter />
     </div>
   );
 }
